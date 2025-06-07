411bdad767be6748b4960e3800f3ef2f37b69b75
RaghavMangat <raghav.coding2000@gmail.com>
2025-06-07 17:29:12 -0500

    Added modulus function.

--- calculator.py
+++ calculator.py
@@ -15,6 +15,9 @@ def divide(a, b):
 def power(a, b):
     return a ^ b
 
+def modulus(a, b):
+    return a % b
+
 if __name__ == "__main__":
     x = int(input("Enter first number: "))
     y = int(input("Enter second number: "))
