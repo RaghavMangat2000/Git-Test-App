764ecc5d916205e21bd22e5f49f4415619ff176d
RaghavMangat <raghav.coding2000@gmail.com>
2025-06-07 17:13:18 -0500

    Added subtract, multiply, divide functions

--- calculator.py
+++ calculator.py
@@ -1,6 +1,17 @@
 def add(a, b):
     return a + b
 
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b != 0:
+        return a / b
+    return "Cannot divide by zero"
+
 if __name__ == "__main__":
     x = int(input("Enter first number: "))
     y = int(input("Enter second number: "))
